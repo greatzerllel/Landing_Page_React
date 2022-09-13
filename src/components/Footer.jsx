6fd7a556da698ac0e044d3d368bb1f61259d1fcb
ZERLLEL <greatzerllel2@gmail.com>
2022-09-13 16:16:53 +0000

    correccion de footer al fin aparece

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,8 @@ import PropTypes from 'prop-types';
 const Footer = ({ footer }) => {
     return (
         <>
-        <h1>HOLA MUNDO2</h1>
         <footer className = "py-5 bg-black">
-            <div className= "container px-5"> <p className="m-0 text-center text-white small">hOLA K ASE{footer.text}</p></div>
+            <div className= "container px-5"> <p className="m-0 text-center text-white small">{footer.text}</p></div>
         </footer>
         </>
     )
