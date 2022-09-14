59e4db5273c028ac6c2db92990887bde14ffbecf
ZERLLEL <greatzerllel2@gmail.com>
2022-09-14 13:58:41 +0000

    use as a temeplate

--- src/data/info.js
+++ src/data/info.js
@@ -1,11 +1,11 @@
 export let navbar = {
-    logo: 'test react',
+    logo: 'test react', //propiedad objeto logo
     options: [
         {id: 1, links: '/#', label: 'Home'},
         {id: 2, links: '/#', label: 'About'},
         {id: 3, links: '/#', label: 'Services'},
         {id: 4, links: '/#', label: 'Contact'},
-    ]
+    ] //propiedad de cta
 }
 
 export let header = {
@@ -14,16 +14,16 @@ export let header = {
     button: {
         link: '/#',
         label: 'learn more'
-     }  
-}
+    }
+}  // objeto dentro de objeto con propiedades y info del header jumbotron
 
 export let cards = [
     { id: 1, photo: 'http://via.placeholder.com/500x325', title: 'Card title', descriptiom: 'Lorem ipsum dolor sit amet, consectetur', btn: 'boton'},
     { id: 2, photo: 'http://via.placeholder.com/500x325', title: 'Card title', descriptiom: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod', btn: 'boton'},
     { id: 3, photo: 'http://via.placeholder.com/500x325', title: 'Card title', descriptiom: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ', btn: 'boton'},
     { id: 4, photo: 'http://via.placeholder.com/500x325', title: 'Card title', descriptiom: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed ', btn: 'boton'}
-]
+] //propiedad objeto dentro de array de cards
 
 export let footer = {
-    text: 'Copyright bla bla bla 2022'
-}
+    text: 'HELLO WORLD!!, ITS WORKING'
+} //propiedad objeto de footer
